9ef1e08aadbbb0fcc35dd8112cdcbf46dd03ccfc
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-04-02 22:34:35 +0300

    Add files via upload

---
+++ 05. Login/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _05._Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = Console.ReadLine();
+            int numbersOfAttempts = 4;
+            string reversedUserName = string.Empty;
+            int counter = 4;
+
+            for (int j = userName.Length - 1; j >= 0; j--)
+            {
+                reversedUserName += userName[j];
+            }
+
+            
+            for (int i = 1; i <= numbersOfAttempts; i++)
+            {
+                
+                string password = Console.ReadLine();
+                counter--;
+
+                if (password == reversedUserName)
+                {
+                    Console.WriteLine($"User {userName} logged in.");
+                    break;
+                }
+                else if (counter != 0)
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+                if (counter == 0)
+                {
+                    Console.WriteLine($"User {userName} blocked!");
+                    break;
+                }
+
+            }
+        }
+    }
+}
