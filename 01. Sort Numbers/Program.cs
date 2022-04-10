95d3749bcda817a5d8b9d5b2ed94892708385fb0
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-04-10 16:53:28 +0300

    Add files via upload

---
+++ 01. Sort Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Sort_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfNumbers = 3;
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < countOfNumbers; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                numbers.Add(n);
+            }
+            Console.WriteLine(string.Join(Environment.NewLine , numbers.OrderByDescending(x => x)));
+            
+        }
+    }
+}
