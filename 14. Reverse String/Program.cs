ab10691fb824b0daef6153f31db19c8db14d8c9e
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-04-10 19:10:04 +0300

    Add files via upload

---
+++ 14. Reverse String/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _14._Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string reverseInput = string.Empty;
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                reverseInput += input[i];
+            }
+            Console.WriteLine(reverseInput);
+        }
+    }
+}
