a1e9d8444fd118254970d4c038bd6e4399087ccf
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-10-16 19:29:07 +0300

    Update Program.cs

--- 06. Strong number/Program.cs
+++ 06. Strong number/Program.cs
@@ -8,14 +8,13 @@ namespace Demo
         {
             string num = Console.ReadLine();
             int currNum = 0;
-
             int isStrongNum = 0;
 
             for (int i = 0; i < num.Length; i++)
             {
                 currNum = num[i] - '0';
-
                 int factoriel = 1;
+                
                 for (int j = 1; j <= currNum; j++)
                 {
                     factoriel *= j;
