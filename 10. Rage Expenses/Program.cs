5f876fa3bba59f8dcc009dfa6548a1b3dafe3d3c
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-10-16 19:21:57 +0300

    Update Program.cs

--- 10. Rage Expenses/Program.cs
+++ 10. Rage Expenses/Program.cs
@@ -1,12 +1,12 @@
-﻿using System;
+using System;
 
-namespace _10._Rage_Expenses
+namespace Demo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int lostGamesCount = int.Parse(Console.ReadLine());
+            int lostGames = int.Parse(Console.ReadLine());
             double headsetPrice = double.Parse(Console.ReadLine());
             double mousePrice = double.Parse(Console.ReadLine());
             double keyboardPrice = double.Parse(Console.ReadLine());
@@ -14,31 +14,26 @@ namespace _10._Rage_Expenses
 
             double rageExpenses = 0;
 
-            double totalHeadsetPrice = (lostGamesCount / 2) * headsetPrice;
-            double totalMousePrice = (lostGamesCount / 3) * mousePrice;
-            double totalKeyboardPrice = (lostGamesCount / 6) * keyboardPrice;
-            double totalDisplayPrice = (lostGamesCount / 12) * displayPrice;
-
-            if (lostGamesCount >= 12)
+            for (int i = 2; i <= lostGames; i++)
             {
-                rageExpenses = totalHeadsetPrice + totalMousePrice + totalKeyboardPrice + totalDisplayPrice;
+                if (i % 12 == 0)
+                {
+                    rageExpenses += headsetPrice + mousePrice + keyboardPrice + displayPrice;
+                }
+                else if (i % 6 == 0)
+                {
+                    rageExpenses += headsetPrice + mousePrice + keyboardPrice;
+                }
+                else if (i % 3 == 0)
+                {
+                    rageExpenses += mousePrice;
+                }
+                else if (i % 2 == 0)
+                {
+                    rageExpenses += headsetPrice;
+                }
             }
-            else if (lostGamesCount < 12 && totalKeyboardPrice > 3)
-            {
-                rageExpenses = totalHeadsetPrice + totalMousePrice + totalKeyboardPrice;
-                
-            }
-            else if (lostGamesCount == 3)
-            {
-                rageExpenses = totalHeadsetPrice + totalMousePrice;
-            }
-            else if (lostGamesCount == 2)
-            {
-                rageExpenses = totalHeadsetPrice;
-            }
-
             Console.WriteLine($"Rage expenses: {rageExpenses:f2} lv.");
-
         }
     }
 }
