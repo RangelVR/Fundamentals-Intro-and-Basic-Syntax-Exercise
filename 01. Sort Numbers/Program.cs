caa0ce48af61d52fbbce407404daae7b5679838f
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-10-16 23:16:39 +0300

    Update Program.cs

--- 01. Sort Numbers/Program.cs
+++ 01. Sort Numbers/Program.cs
@@ -1,23 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System;
 
-namespace _01._Sort_Numbers
+namespace demo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int countOfNumbers = 3;
-            List<int> numbers = new List<int>();
+            int n1 = int.MinValue;
+            int n2 = int.MinValue;
+            int n3 = int.MinValue;
 
-            for (int i = 0; i < countOfNumbers; i++)
+            for (int i = 0; i < 3; i++)
             {
-                int n = int.Parse(Console.ReadLine());
-                numbers.Add(n);
+                int inputNum = int.Parse(Console.ReadLine());
+
+                if (inputNum > n1)
+                {
+                    n3 = n2;
+                    n2 = n1;
+                    n1 = inputNum;
+                }
+                else if (inputNum > n2)
+                {
+                    n3 = n2;
+                    n2 = inputNum;
+                }
+                else if (inputNum > n3)
+                {
+                    n3 = inputNum;
+                }
             }
-            Console.WriteLine(string.Join(Environment.NewLine , numbers.OrderByDescending(x => x)));
-            
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
         }
     }
 }
