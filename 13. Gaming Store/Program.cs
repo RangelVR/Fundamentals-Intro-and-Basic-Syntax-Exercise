bfb78128e33604773a83c4889caf01b1f94a402e
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-10-17 21:56:19 +0300

    Update Program.cs

--- 13. Gaming Store/Program.cs
+++ 13. Gaming Store/Program.cs
@@ -1,112 +1,146 @@
-﻿using System;
+using System;
 
-namespace _13._Gaming_Store
+namespace demo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double currBalance = double.Parse(Console.ReadLine());
-            
-            double outFallPrice = 39.99;
-            double csPricePrice = 15.99;
-            double zplinterPrice = 19.99;
-            double honoredPrice = 59.99;
-            double roverWatchPrice = 29.99;
-            double roverWatchOrEdPrice = 39.99;
-            
-            string gameName = Console.ReadLine();
-            
-            double totalSpend = 0;
+            double OUTFALL4 = 39.99;
+            double CSOG = 15.99;
+            double ZPLINTERzELL = 19.99;
+            double HONORED2 = 59.99;
+            double ROVERwATCH = 29.99;
+            double ROVERwATCHoRIGINS = 39.99;
 
-            while (gameName != "Game Time")
+            double money = double.Parse(Console.ReadLine());
+            string command = String.Empty;
+            double spendMoney = 0;
+
+            while ((command = Console.ReadLine()) != "Game Time")
             {
                 
-                switch (gameName)
+                switch (command)
                 {
                     case "OutFall 4":
-                        if (outFallPrice > currBalance)
+                        if (money >= OUTFALL4)
+                        {
+                            Console.WriteLine("Bought OutFall 4");
+                            money -= OUTFALL4;
+                            spendMoney += OUTFALL4;
+                        }
+                        else
                         {
                             Console.WriteLine("Too Expensive");
+                        }
+                        if (money == 0)
+                        {
+                            Console.WriteLine("Out of money!");
                             break;
                         }
-                        currBalance -= outFallPrice;
-                        totalSpend += outFallPrice;
-                        Console.WriteLine("Bought OutFall 4");
                         break;
 
                     case "CS: OG":
-                        if (csPricePrice > currBalance)
+                        if (money >= CSOG)
+                        {
+                            Console.WriteLine("Bought CS: OG");
+                            money -= CSOG;
+                            spendMoney += CSOG;
+                        }
+                        else
                         {
                             Console.WriteLine("Too Expensive");
+                        }
+                        if (money == 0)
+                        {
+                            Console.WriteLine("Out of money!");
                             break;
                         }
-                        currBalance -= csPricePrice;
-                        totalSpend += csPricePrice;
-                        Console.WriteLine("Bought CS: OG");
                         break;
 
                     case "Zplinter Zell":
-                        if (zplinterPrice > currBalance)
+                        if (money >= ZPLINTERzELL)
+                        {
+                            Console.WriteLine("Bought Zplinter Zell");
+                            money -= ZPLINTERzELL;
+                            spendMoney += ZPLINTERzELL;
+                        }
+                        else
                         {
                             Console.WriteLine("Too Expensive");
+                        }
+                        if (money == 0)
+                        {
+                            Console.WriteLine("Out of money!");
                             break;
                         }
-                        currBalance -= zplinterPrice;
-                        totalSpend += zplinterPrice;
-                        Console.WriteLine("Bought Zplinter Zell");
                         break;
 
                     case "Honored 2":
-                        if (honoredPrice > currBalance)
+                        if (money >= HONORED2)
+                        {
+                            Console.WriteLine("Bought Honored 2");
+                            money -= HONORED2;
+                            spendMoney += HONORED2;
+                        }
+                        else
                         {
                             Console.WriteLine("Too Expensive");
+                        }
+                        if (money == 0)
+                        {
+                            Console.WriteLine("Out of money!");
                             break;
                         }
-                        currBalance -= honoredPrice;
-                        totalSpend += honoredPrice;
-                        Console.WriteLine("Bought Honored 2");
                         break;
 
                     case "RoverWatch":
-                        if (roverWatchPrice > currBalance)
+                        if (money >= ROVERwATCH)
+                        {
+                            Console.WriteLine("Bought RoverWatch");
+                            money -= ROVERwATCH;
+                            spendMoney += ROVERwATCH;
+                        }
+                        else
                         {
                             Console.WriteLine("Too Expensive");
+                        }
+                        if (money == 0)
+                        {
+                            Console.WriteLine("Out of money!");
                             break;
                         }
-                        currBalance -= roverWatchPrice;
-                        totalSpend += roverWatchPrice;
-                        Console.WriteLine("Bought RoverWatch");
                         break;
 
                     case "RoverWatch Origins Edition":
-                        if (roverWatchOrEdPrice > currBalance)
+                        if (money >= ROVERwATCHoRIGINS)
+                        {
+                            Console.WriteLine("Bought RoverWatch Origins Edition");
+                            money -= ROVERwATCHoRIGINS;
+                            spendMoney += ROVERwATCHoRIGINS;
+                        }
+                        else
                         {
                             Console.WriteLine("Too Expensive");
+                        }
+                        if (money == 0)
+                        {
+                            Console.WriteLine("Out of money!");
                             break;
                         }
-                        currBalance -= roverWatchOrEdPrice;
-                        totalSpend += roverWatchOrEdPrice;
-                        Console.WriteLine("Bought RoverWatch Origins Edition");
                         break;
 
                     default:
                         Console.WriteLine("Not Found");
                         break;
                 }
-                gameName = Console.ReadLine();
-                
             }
 
-            if (currBalance > 0)
+            if (money > 0)
             {
-                Console.WriteLine($"Total spent: ${totalSpend:f2}. Remaining: ${currBalance:f2}");
+                Console.WriteLine($"Total spent: ${spendMoney:f2}. Remaining: ${money:f2}");
             }
-            else
-            {
-                Console.WriteLine("Out of money!");
-            }
-            
+
         }
     }
 }
