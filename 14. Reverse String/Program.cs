55200abf8206093cc98d7f36b2ccea5bd6015b44
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-10-17 22:03:30 +0300

    Update Program.cs

--- 14. Reverse String/Program.cs
+++ 14. Reverse String/Program.cs
@@ -1,6 +1,6 @@
-﻿using System;
+using System;
 
-namespace _14._Reverse_String
+namespace demo
 {
     class Program
     {
@@ -8,13 +8,11 @@ namespace _14._Reverse_String
         {
             string input = Console.ReadLine();
 
-            string reversedInput = string.Empty;
-
             for (int i = input.Length - 1; i >= 0; i--)
             {
-                reverseInput += input[i];
+                Console.Write(input[i]);
             }
-            Console.WriteLine(reverseInput);
+
         }
     }
 }
