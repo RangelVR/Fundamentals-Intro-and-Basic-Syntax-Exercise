4a8eb84be0f43f57672f11829410abd496b32f11
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-10-19 22:05:10 +0300

    Update Program.cs

--- 05. Messages/Program.cs
+++ 05. Messages/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace _05._Messages
 {
@@ -6,31 +6,184 @@ namespace _05._Messages
     {
         static void Main(string[] args)
         {
-            int clicks = int.Parse(Console.ReadLine());
-            string message = string.Empty;
 
-            for (int i = 0; i < clicks; i++)
+            int numInputs = int.Parse(Console.ReadLine());
+            string word = string.Empty;
+
+            for (int i = 0; i < numInputs; i++)
             {
-                string digits = Console.ReadLine();
-                int digitLength = digits.Length;
-                int digit = digits[0] - '0';    
-                int offset = (digit - 2) * 3;
+                string num = Console.ReadLine();
 
-                if (digit == 0)
+                switch (num[0])
                 {
-                    message += (char)(digit + 32);
-                    continue;
-                }
+                    case '0':
+                        word += ' ';
+                        break;
 
-                if (digit == 7 || digit == 9)
-                {
-                    offset++;
+                    case '2':
+                        //additionalNum = 47;
+                        if (num.Length == 1)
+                        {
+                            word += (char)('2' + '/');
+                        }
+                        else if (num.Length == 2)
+                        {
+                            word += (char)('3' + '/');
+                        }
+                        else if (num.Length == 3)
+                        {
+                            word += (char)('4' + '/');
+                        }
+                        break;
+
+                    case '3':
+                        //additionalNum = 49;
+                        if (num.Length == 1)
+                        {
+                            word += (char)('3' + '1');
+                        }
+                        else if (num.Length == 2)
+                        {
+                            word += (char)('4' + '1');
+                        }
+                        else if (num.Length == 3)
+                        {
+                            word += (char)('5' + '1');
+                        }
+                        break;
+
+                    case '4':
+                        //additionalNum = 51;
+                        if (num.Length == 1)
+                        {
+                            word += (char)('4' + '3');
+                        }
+                        else if (num.Length == 2)
+                        {
+                            word += (char)('5' + '3');
+                        }
+                        else if (num.Length == 3)
+                        {
+                            word += (char)('6' + '3');
+                        }
+                        break;
+
+                    case '5':
+                        //additionalNum = 53;
+                        if (num.Length == 1)
+                        {
+                            word += (char)('5' + '5');
+                        }
+                        else if (num.Length == 2)
+                        {
+                            word += (char)('6' + '5');
+                        }
+                        else if (num.Length == 3)
+                        {
+                            word += (char)('7' + '5');
+                        }
+                        break;
+
+                    case '6':
+                        //additionalNum = 55;
+                        if (num.Length == 1)
+                        {
+                            word += (char)('6' + '7');
+                        }
+                        else if (num.Length == 2)
+                        {
+                            word += (char)('7' + '7');
+                        }
+                        else if (num.Length == 3)
+                        {
+                            word += (char)('8' + '7');
+                        }
+                        break;
+
+                    case '7':
+                        //additionalNum = 57;
+                        if (num.Length == 1)
+                        {
+                            word += (char)('7' + '9');
+                        }
+                        else if (num.Length == 2)
+                        {
+                            word += (char)('8' + '9');
+                        }
+                        else if (num.Length == 3)
+                        {
+                            word += (char)('9' + '9');
+                        }
+                        else if (num.Length == 4)
+                        {
+                            word += (char)(':' + '9');
+                        }
+                        break;
+
+                    case '8':
+                        //additionalNum = 59;
+                        if (num.Length == 1)
+                        {
+                            word += (char)('8' + '<');
+                        }
+                        else if (num.Length == 2)
+                        {
+                            word += (char)('9' + '<');
+                        }
+                        else if (num.Length == 3)
+                        {
+                            word += (char)(':' + '<');
+                        }
+                        break;
+
+                    case '9':
+                        //additionalNum = 61;
+                        if (num.Length == 1)
+                        {
+                            word += (char)('9' + '>');
+                        }
+                        else if (num.Length == 2)
+                        {
+                            word += (char)(':' + '>');
+                        }
+                        else if (num.Length == 3)
+                        {
+                            word += (char)(';' + '>');
+                        }
+                        else if (num.Length == 3)
+                        {
+                            word += (char)('<' + '>');
+                        }
+                        break;
                 }
-                int letterIndex = offset + digitLength - 1;
-                message += (char)(letterIndex + 97);
             }
 
-            Console.WriteLine(message);
+            Console.WriteLine(word);
+            //int clicks = int.Parse(Console.ReadLine());
+            //string message = string.Empty;
+
+            //for (int i = 0; i < clicks; i++)
+            //{
+            //    string digits = Console.ReadLine();
+            //    int digitLength = digits.Length;
+            //    int digit = digits[0] - '0';    
+            //    int offset = (digit - 2) * 3;
+
+            //    if (digit == 0)
+            //    {
+            //        message += (char)(digit + 32);
+            //        continue;
+            //    }
+
+            //    if (digit == 8 || digit == 9)
+            //    {
+            //        offset++;
+            //    }
+            //    int letterIndex = offset + digitLength - 1;
+            //    message += (char)(letterIndex + 97);
+            //}
+
+            //Console.WriteLine(message);
 
         }
     }
